41a725ecfffb1a0b6bf926e977f45e7e5b7f521d
aqie123 <2924811900@qq.com>
2017-08-16 07:57:42 +0800

    2017/8/16

--- protoss/pages/category/category.js
+++ protoss/pages/category/category.js
@@ -8,7 +8,9 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    transClassArr: ['tanslate0', 'tanslate1', 'tanslate2', 'tanslate3', 'tanslate4', 'tanslate5'],
+    currentMenuIndex: 0,
+    loadingHidden: false,
   },
 
   /**
@@ -18,13 +20,100 @@ Page({
     this._loadData();
   },
   _loadData:function(){
+    var that = this;
     category.getCategoryType((categoryData)=>{
+      // 加载分类数据
       that.setData({
         categoryTypeArr: categoryData,
         loadingHidden: true
       });
+
+      // 必须在回调里面获取分类信息
+      that.getProductsByCategory(categoryData[0].id, (data) => {
+        var dataObj = {
+          procucts: data,
+          topImgUrl: categoryData[0].img.url,
+          title: categoryData[0].name
+        };
+        that.setData({
+          loadingHidden: true,
+          categoryInfo0: dataObj
+        });
+        // callback && callback();
+      });
     })
+  },
+
+  getProductsByCategory: function (id, callback) {
+    category.getProductsByCategory(id, (data) => {
+      callback && callback(data);
+    });
+  },
+
+  /*切换分类*/
+  changeCategory: function (event) {
+    var index = category.getDataSet(event, 'index'),
+      id = category.getDataSet(event, 'id')//获取data-set
+    this.setData({
+      currentMenuIndex: index
+    });
+
+    //如果数据是第一次请求
+    if (!this.isLoadedData(index)) {
+      var that = this;
+      this.getProductsByCategory(id, (data) => {
+        that.setData(that.getDataObjForBind(index, data));
+      });
+    }
+  },
+
+  isLoadedData: function (index) {
+    if (this.data['categoryInfo' + index]) {
+      return true;
+    }
+    return false;
+  },
+
+  getDataObjForBind: function (index, data) {
+    var obj = {},
+      arr = [0, 1, 2, 3, 4, 5],
+      baseData = this.data.categoryTypeArr[index];
+    for (var item in arr) {
+      if (item == arr[index]) {
+        obj['categoryInfo' + item] = {
+          procucts: data,
+          topImgUrl: baseData.img.url,
+          title: baseData.name
+        };
+
+        return obj;
+      }
+    }
+  },
+
+  /*跳转到商品详情*/
+  onProductsItemTap: function (event) {
+    var id = category.getDataSet(event, 'id');
+    wx.navigateTo({
+      url: '../product/product?id=' + id
+    })
+  },
+
+  /*下拉刷新页面*/
+  onPullDownRefresh: function () {
+    this._loadData(() => {
+      wx.stopPullDownRefresh()
+    });
+  },
+
+  //分享效果
+  onShareAppMessage: function () {
+    return {
+      title: '零食商贩 Pretty Vendor',
+      path: 'pages/category/category'
+    }
   }
+
 
  
 })
